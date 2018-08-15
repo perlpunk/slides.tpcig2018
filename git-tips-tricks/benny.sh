1e415969b1bff3b5806e897360067422679b4ee6
Tina Müller <cpan2@tinita.de>
2018-08-16 00:52:33 +0200

    ...

--- git-tips-tricks/benny.sh
+++ git-tips-tricks/benny.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
+cd ~/tpcig2018/benny/project
 git config --local user.email "benny@example.org"
 git config --local user.name "Benny"
 RPROMPT=""
-cd ~/tpcig2018/benny/project
--- git-tips-tricks/joon.sh
+++ git-tips-tricks/joon.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
+cd ~/tpcig2018/joon/project
 git config --local user.email "joon@example.org"
 git config --local user.name "Joon"
 RPROMPT=""
-cd ~/tpcig2018/joon/project
